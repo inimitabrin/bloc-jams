00abf2e2a5902461b6e2193e52723789c18b3178
inimitabrin <breanna.m.bliss@gmail.com>
2017-02-23 19:17:37 -0800

    assignment-22-complete

---
+++ scripts/landing.js
@@ -0,0 +1,12 @@
+var animatePoints = function() {
+                var points = document.getElementsByClassName('point');
+                var revealPoint = function(pointIndex) {
+                        points[pointIndex].style.opacity = 1;
+                        points[pointIndex].style.transform = "scaleX(1) translateY(0)";
+                        points[pointIndex].style.msTransform = "scaleX(1) translateY(0)";
+                        points[pointIndex].style.WebkitTransform = "scaleX(1) translateY(0)";
+                    }
+                for (var i = 0; i<points.length; i++) {
+                    revealPoint(i);
+                }
+             };

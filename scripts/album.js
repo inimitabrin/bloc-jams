6aec05917a55e5d356d1b4833978dd2b4ffb24a1
inimitabrin <breanna.m.bliss@gmail.com>
2017-02-27 12:42:58 -0800

    checkpoint 26 complete

--- scripts/album.js
+++ scripts/album.js
@@ -28,10 +28,24 @@ var albumMarconi = {
      ]
  };
 
+ var albumCats = {
+     title: 'My Cats',
+     artist: 'Brin B',
+     label: 'homerecords',
+     year: '1994',
+     albumArtUrl: 'assets/images/album_covers/03.png',
+     songs: [
+         { title: 'Gigi', duration: '4:00' },
+         { title: 'Bilbo Baggins', duration: '3:50' },
+         { title: 'Cashmere', duration: '2:50'},
+         { title: 'Jasmine/Star', duration: '3:30' },
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -41,7 +55,7 @@ var albumMarconi = {
  };
 
  var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
@@ -55,7 +69,23 @@ var albumMarconi = {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+ var songRows = document.getElementsByClassName('album-view-song-item');
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     songListContainer.addEventListener('mouseover', function(event) {
+      if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      }
+     });
+
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
  };

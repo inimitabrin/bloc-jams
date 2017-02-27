18806b21358206d6d95db7c94d036f257809c080
inimitabrin <breanna.m.bliss@gmail.com>
2017-02-27 10:01:43 -0800

    checkpoint 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,24 @@
-var animatePoints = function() {
-                var points = document.getElementsByClassName('point');
-                var revealPoint = function(pointIndex) {
-                        points[pointIndex].style.opacity = 1;
-                        points[pointIndex].style.transform = "scaleX(1) translateY(0)";
-                        points[pointIndex].style.msTransform = "scaleX(1) translateY(0)";
-                        points[pointIndex].style.WebkitTransform = "scaleX(1) translateY(0)";
-                    }
-                for (var i = 0; i<points.length; i++) {
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+        var revealPoint = function(index) {
+                points[index].style.opacity = 1;
+                points[index].style.transform = "scaleX(1) translateY(0)";
+                points[index].style.msTransform = "scaleX(1) translateY(0)";
+                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+            }
+        for (var i = 0; i<points.length; i++) {
                     revealPoint(i);
                 }
              };
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}

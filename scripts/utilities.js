de3356f77c1298274616990ea81f27d64d202a8b
inimitabrin <breanna.m.bliss@gmail.com>
2017-02-27 10:22:50 -0800

    assignment 23 complete

---
+++ scripts/utilities.js
@@ -0,0 +1,15 @@
+var forEach = function(points) {
+  points = document.getElementsByClassName('point')
+  for (var i=0; i<points.length; i++) {
+    points[i], i;
+  }
+  var revealPoint = function(index) {
+                points[index].style.opacity = 1;
+                points[index].style.transform = "scaleX(1) translateY(0)";
+                points[index].style.msTransform = "scaleX(1) translateY(0)";
+                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+
+
+forEach(revealPoint);
